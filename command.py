b2b298b8ffd3c94c596e246c39e84bc882ea7ed8
Uladzislau Valashchuk <uladzislau.valashchuk@ah.nl>
2020-10-03 23:46:23 +0300

    add separator constant

--- command.py
+++ command.py
@@ -3,7 +3,7 @@ from common.crypt import encrypt_aes, decrypt_aes
 from common.socket_util import decode_utf8, AES_ENCODED, NO_INPUT, NOT_FOUND
 from server import Session
 
-storage_path = 'storage/'
+COMMAND_SEPARATOR = "-||-"
 
 
 def perform_by_name(name: str, session: Session):
@@ -34,7 +34,7 @@ def perform_get(session: Session):
 def perform_new(session: Session):
     session.sock.send_string("Please, enter filename to create.")
     file = decode_utf8(session.sock.recv().body)
-    session.sock.send_string("!editor file ")
+    session.sock.send_string("!editor{}file{}".format(COMMAND_SEPARATOR, COMMAND_SEPARATOR))
     raw = session.sock.recv().body
     data = decrypt_aes(session.key, raw)
     storage.create_for_user(session.username, file, data)
@@ -45,7 +45,9 @@ def perform_edit(session: Session):
     file = decode_utf8(session.sock.recv().body)
     try:
         contents = storage.get_for_user(session.username, file)
-        session.sock.send("!editor-||-{}-||-".format(session.username).encode('utf-8') + encrypt_aes(session.key, contents))
+        session.sock.send(
+            "!editor{}{}{}".format(COMMAND_SEPARATOR, session.username, COMMAND_SEPARATOR).encode(
+                'utf-8') + encrypt_aes(session.key, contents))
         raw = session.sock.recv().body
         data = decrypt_aes(session.key, raw)
         storage.create_for_user(session.username, file, data)
